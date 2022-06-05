af2228712ffe7a53d064e83adeb354bafb5ca2c5
Kirk <kirk.douglas.richardson@gmail.com>
2022-06-04 22:38:21 -0600

    Fix indexing bug in symbol table; add toString.

--- bin/symbol_table.dart
+++ bin/symbol_table.dart
@@ -1,5 +1,3 @@
-import 'constants.dart';
-
 abstract class ISymbolTable {
   /// Empties the symbol table, and resets the four indices to 0.
   /// Should be called when starting to compile a subroutine declaration.
@@ -10,9 +8,9 @@ abstract class ISymbolTable {
   /// 1 to the index.
   void define(String name, String type, String kind);
 
-  /// Returns the number of variables of the given kind already defined
-  /// in the table.
-  int varCount(String kind);
+  // /// Returns the number of variables of the given kind already defined
+  // /// in the table.
+  // int varCount(String kind);
 
   /// Returns the kind of the named identifier (STATIC, FIELD, ARG, VAR, NONE)
   String kindOf(String name);
@@ -20,7 +18,7 @@ abstract class ISymbolTable {
   /// Returns the type of the named variable.
   String typeOf(String name);
 
-  /// Returns the index of the named variable.
+  /// Returns the index of the named variable, or -1 if not found.
   int indexOf(String name);
 }
 
@@ -38,8 +36,11 @@ class _VarInfo {
 }
 
 class SymbolTable implements ISymbolTable {
+  /// A variable name to metadata map.
   final Map<String, _VarInfo> _table = {};
-  final _indexOfKind = {
+
+  /// Tracks counts of variables by kind.
+  final _countForKind = {
     'static': 0,
     'field': 0,
     'arg': 0,
@@ -55,16 +56,23 @@ class SymbolTable implements ISymbolTable {
     // NOTE - currently does not handle duplicate var exceptions.
     // Closest scope will win.
 
+    final prevCount = _countForKind[kind];
+    if (prevCount == null) {
+      throw Exception('Invalid kind $kind for variable $name');
+    }
+
     _table[name] = _VarInfo(
       name: name,
       type: type,
       kind: kind,
-      index: varCount(kind),
+      index: prevCount, // Index is 0-based, lags count by 1.
     );
+
+    _countForKind[kind] = prevCount + 1;
   }
 
   @override
-  int indexOf(String name) => _table[name]?.index ?? 0;
+  int indexOf(String name) => _table[name]?.index ?? -1;
 
   @override
   String kindOf(String name) {
@@ -75,8 +83,8 @@ class SymbolTable implements ISymbolTable {
   @override
   void reset() {
     _table.clear();
-    resetIndex(k) => _indexOfKind[k] = 0;
-    _indexOfKind.keys.forEach(resetIndex);
+    resetIndex(k) => _countForKind[k] = 0;
+    _countForKind.keys.forEach(resetIndex);
   }
 
   @override
@@ -86,7 +94,19 @@ class SymbolTable implements ISymbolTable {
   }
 
   @override
-  int varCount(String kind) => _indexOfKind[kind]!;
+  String toString() {
+    final b = StringBuffer();
+    void writeValue(_VarInfo e) {
+      b.writeln(
+          '{name: ${e.name}, kind: ${e.kind}, type: ${e.type}, index: ${e.index}');
+    }
+
+    _table.values.forEach(writeValue);
+    return b.toString();
+  }
+
+  // @override
+  // int varCount(String kind) => _countForKind[kind] ?? -1;
 
   //////////////////////////////////////////////////////////////////////////////
   //////////////////       Private Utility      ////////////////////////////////
